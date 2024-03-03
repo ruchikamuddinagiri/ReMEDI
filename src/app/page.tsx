ca3589a45e02f24f322f7a337f58576688ea7805
ruchikamuddinagiri <rrmuddinagiri@gmail.com>
2024-03-03 05:05:22 -0500

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     // Attempt to retrieve the session token from sessionStorage
     console.log("hi")
     const sessionToken = sessionStorage.getItem('email');
-    if(sessionToken){
-      setShowModal(false)
+    if(!sessionToken){
+      setShowModal(true)
     }
    console.log('email:', sessionToken);
   }, []);
@@ -85,7 +85,6 @@ export default function Home() {
         <a
           href="/text"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -100,9 +99,9 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/document"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -119,7 +118,7 @@ export default function Home() {
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
