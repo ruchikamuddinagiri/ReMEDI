14f2a3f22d0af5963a3b7d1014c04a6f5f5723d0
ruchikamuddinagiri <rrmuddinagiri@gmail.com>
2024-03-03 00:00:10 -0500

    modal added

---
+++ src/app/api/route.ts
@@ -0,0 +1,46 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+
+
+export async function POST(request: Request){
+
+  const user = await request.json()
+  let email = user.email
+  console.log(email)
+
+  // save to dynamodb
+
+  const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  });
+
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+    try {
+      await ddbDocClient.send(new PutCommand({
+        TableName: 'users',
+        Item: user,
+      }));
+
+      return new Response(JSON.stringify(user), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        status: 201,
+      })
+    } catch (error) {
+      console.error('DynamoDB Error:', error);
+      return new Response(JSON.stringify({ error: 'Failed to store item in DynamoDB', item: user }), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        status: 500,
+      })
+    }
+  
+
+}
